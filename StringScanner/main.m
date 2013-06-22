276c621c1b2591968dc2c38fd0d6e4ddd646c0e0
Douglas Hill <hello@douglashill.co>
2013-06-23 00:20:45 +0100

    Add basic strings file parsing.

--- StringScanner/main.m
+++ StringScanner/main.m
@@ -8,14 +8,65 @@
 
 #import <Foundation/Foundation.h>
 
+
+
+@interface NSScanner (DHAdditions)
+
+- (BOOL)scanPastString:(NSString *)stopString;
+- (BOOL)scanPastString:(NSString *)stopString intoString:(NSString **)outString;
+- (BOOL)scanBetweenString:(NSString *)startString andString:(NSString *)endString intoString:(NSString **)outString;
+
+@end
+
+@implementation NSScanner (DHAdditions)
+
+- (BOOL)scanPastString:(NSString *)stopString
+{
+	return [self scanPastString:stopString intoString:NULL];
+}
+
+- (BOOL)scanPastString:(NSString *)stopString intoString:(NSString **)outString;
+{
+	[self scanUpToString:stopString intoString:outString];
+	return [self scanString:stopString intoString:NULL];
+}
+
+- (BOOL)scanBetweenString:(NSString *)startString andString:(NSString *)endString intoString:(NSString **)outString
+{
+	[self scanPastString:startString];
+	return [self scanPastString:endString intoString:outString];
+}
+
+@end
+
+
+
 int main(int argc, const char * argv[])
 {
-
 	@autoreleasepool {
-	    
-	    // insert code here...
-	    NSLog(@"Hello, World!");
-	    
+		
+		NSString *file = [NSString stringWithContentsOfFile:@"/Users/Douglas/Desktop/Localizable.strings" usedEncoding:NULL error:NULL];
+		NSScanner *scanner = [NSScanner scannerWithString:file];
+		
+		while ([scanner isAtEnd] == NO) {
+			NSString *comment;
+			NSString *key;
+			NSString *value;
+			
+			[scanner scanBetweenString:@"/*" andString:@"*/" intoString:&comment];
+			comment = [comment stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+			
+			[scanner scanBetweenString:@"\"" andString:@"\"" intoString:&key];
+			BOOL equalsCorrect = [scanner scanString:@"=" intoString:NULL];
+			[scanner scanBetweenString:@"\"" andString:@"\"" intoString:&value];
+			BOOL terminatorCorrect = [scanner scanString:@";" intoString:NULL];
+			
+			if ([comment isEqualToString:key] || [comment isEqualToString:value])
+				NSLog(@"%@ : %@ (%d, %d)", key, value, equalsCorrect, terminatorCorrect);	
+			else
+				NSLog(@"%@ : %@ (%@, %d, %d)", key, value, comment, equalsCorrect, terminatorCorrect);
+		}
+		
 	}
     return 0;
 }
