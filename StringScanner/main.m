394cf8243b2fd29448c04a84ffef0be2344ce27b
Douglas Hill <hello@douglashill.co>
2013-06-22 23:16:07 +0100

    Initial Commit

---
+++ StringScanner/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StringScanner
+//
+//  Created by Douglas Hill on 22/06/2013.
+//  Copyright (c) 2013 Douglas Hill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
